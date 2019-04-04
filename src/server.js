ff868a2c1ec26f9e021ff157e6ab67db95c34a2e
Sarah Flynn <sarah.flynn@icloud.com>
2019-04-03 19:37:36 -0700

    added express, serving up api at graphql endpoint with graphiql

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
-import { graphql, buildSchema } from 'graphql';
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
 
 const schema = buildSchema(`
   type Query {
@@ -12,6 +14,12 @@ const root = {
   },
 };
 
-graphql(schema, '{ hello }', root).then(response => {
-  console.log(response);
-});
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
